f3dd9108d7e8680a0168f1e5d8c91d920cd689d3
Greg Jones <gjones@smartling.com>
2012-03-19 15:01:50 -0400

    SmartlingApiExample for using the Smartling API.

---
+++ api-sdk/example/SmartlingApiExample.java
@@ -0,0 +1,58 @@
+/* Copyright 2012 Smartling, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this work except in compliance with the License.
+ * You may obtain a copy of the License in the LICENSE file, or at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import com.smartling.api.sdk.file.*;
+import com.smartling.api.sdk.file.response.*;
+import java.io.File;
+import org.apache.commons.io.FilenameUtils;
+
+public class SmartlingApiExample
+{
+    private static final String API_KEY       = "YOUR-API-KEY";
+    private static final String PROJECT_ID    = "YOUR-PROJECT-D";
+    private static final String LOCALE        = "YOUR-LOCALE";
+
+    private static final String PATH_TO_FILE  = "resources/test.properties";
+    private static final String FILE_ENCODING = "UTF-8";
+    private static final String FILE_TYPE     = "javaProperties";
+
+    public static void main(String args[]) throws FileApiException
+    {
+        FileApiClientAdapter smartlingFAPI = new FileApiClientAdapterImpl(API_KEY, PROJECT_ID);
+
+        // upload the file
+        File file = new File(FilenameUtils.separatorsToSystem(PATH_TO_FILE));
+        ApiResponse<UploadData> uploadFileResponse = smartlingFAPI.uploadFile(FILE_TYPE, getFileUri(file), file, false, FILE_ENCODING);
+        System.out.println(uploadFileResponse);
+
+        // run a search for files
+        FileListSearchParams fileListSearchParams = new FileListSearchParams();
+        fileListSearchParams.setUriMask(getFileUri(file));
+        ApiResponse<FileList> filesListResponse = smartlingFAPI.getFilesList(fileListSearchParams);
+        System.out.println(filesListResponse);
+
+        // check the file status
+        ApiResponse<FileStatus> fileStatusResponse = smartlingFAPI.getFileStatus(getFileUri(file), LOCALE);
+        System.out.println(fileStatusResponse);
+
+        // get the file back, including any translations that have been published.
+        String translatedContent = smartlingFAPI.getFile(getFileUri(file), LOCALE);
+        System.out.println(translatedContent);
+    }
+
+    private static String getFileUri(File file)
+    {
+        return file.getName();
+    }
+}
