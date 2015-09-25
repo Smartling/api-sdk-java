4a232e71bf683761995342bff0ab6a72307b7538
Pavlo Myrotiuk <pmyrotiuk@smartling.com>
2015-09-25 16:30:52 +0300

    #5 refactored class ProjectPropertiesHolder class according to code style

--- api-sdk/src/main/java/com/smartling/api/sdk/file/parameters/ProjectPropertiesHolder.java
+++ api-sdk/src/main/java/com/smartling/api/sdk/file/parameters/ProjectPropertiesHolder.java
@@ -8,9 +8,10 @@ import org.apache.commons.logging.LogFactory;
 import java.io.IOException;
 import java.util.Properties;
 
-class ProjectPropertiesHolder {
+class ProjectPropertiesHolder
+{
 
-    private static final Log logger = LogFactory.getLog(ProjectPropertiesHolder.class);
+    private static final Log LOGGER = LogFactory.getLog(ProjectPropertiesHolder.class);
 
     private static final String PROJECT_PROPERTIES_FILE = "sdk-project.properties";
     private static final String PROJECT_VERSION = "version";
@@ -19,26 +20,31 @@ class ProjectPropertiesHolder {
     private static final String CLIENT_KEY = "client";
     private static final String VERSION_KEY = "version";
 
-    static String defaultClientUid() {
+    static String defaultClientUid()
+    {
         return clientUid(Holder.PROPERTIES.getProperty(PROJECT_ARTIFACT_ID), Holder.PROPERTIES.getProperty(PROJECT_VERSION));
     }
 
-    static String clientUid(String name, String version) {
+    static String clientUid(String name, String version)
+    {
         JsonObject object = new JsonObject();
         object.addProperty(CLIENT_KEY, name);
         object.addProperty(VERSION_KEY, version);
         return object.toString();
     }
 
-    private static class Holder {
+    private static class Holder
+    {
         private static final Properties PROPERTIES;
 
         static {
             PROPERTIES = new Properties();
-            try {
+            try
+            {
                 PROPERTIES.load(Holder.class.getClassLoader().getResourceAsStream(PROJECT_PROPERTIES_FILE));
-            } catch (IOException e) {
-                logger.error(String.format("Could not read properties file=%s",PROJECT_PROPERTIES_FILE));
+            } catch (IOException e)
+            {
+                LOGGER.error(String.format("Could not read properties file=%s",PROJECT_PROPERTIES_FILE));
                 throw new RuntimeException(e);
             }
         }
