ab5bf048c130e0d8fd1610d907eb1b43ba353810
Mykhailo Shchukin <mshchukin@smartling.com>
2016-02-01 12:21:43 +0200

    ZD-158 added refreshing token and logging it

--- api-sdk/src/main/java/com/smartling/api/sdk/auth/OAuthTokenProvider.java
+++ api-sdk/src/main/java/com/smartling/api/sdk/auth/OAuthTokenProvider.java
@@ -1,12 +1,18 @@
 package com.smartling.api.sdk.auth;
 
 import com.smartling.api.sdk.exceptions.SmartlingApiException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
 
 public class OAuthTokenProvider implements TokenProvider
 {
     private final String userId;
     private final String userSecret;
     private final AuthApiClient authApiClient;
+    private static final Log LOGGER = LogFactory.getLog(OAuthTokenProvider.class);
 
     private volatile AuthenticationContext authenticationContext;
 
@@ -33,7 +39,27 @@ public class OAuthTokenProvider implements TokenProvider
                 {
                     if (refreshTokenIsValid())
                     {
-                        authenticationContext = authApiClient.refresh(authenticationContext.getRefreshToken()).retrieveData();
+                        final String refreshToken = authenticationContext.getRefreshToken();
+                        try
+                        {
+                            authenticationContext = authApiClient.refresh(refreshToken).retrieveData();
+                        } catch (SmartlingApiException ex){
+                            authenticationContext = null;
+                            LOGGER.warn("Reset refresh token after fail");
+                            JwtConsumer consumer = new JwtConsumerBuilder()
+                                    .setSkipAllDefaultValidators()
+                                    .setRequireExpirationTime()
+                                    .setSkipSignatureVerification()
+                                    .build();
+                            try
+                            {
+                                LOGGER.warn("Failed token info: " + consumer.process(refreshToken).getJwtClaims().getRawJson());
+                            }
+                            catch (InvalidJwtException e)
+                            {
+                                LOGGER.warn("Can't parse refresh token");
+                            }
+                        }
                     }
                     else
                     {
