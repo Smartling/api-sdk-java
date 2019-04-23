409ec1e9c6d3386b215814be4e21dc1adbfbd133
Denis Kurochkin <dkurochkin@smartling.com>
2019-04-23 18:04:46 +0300

    Wrap refreshToken with command

---
+++ api-sdk/src/main/java/com/smartling/api/sdk/auth/RefreshCommand.java
@@ -0,0 +1,43 @@
+package com.smartling.api.sdk.auth;
+
+import java.util.Objects;
+
+public class RefreshCommand
+{
+    private final String refreshToken;
+
+    public RefreshCommand(String refreshToken)
+    {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getRefreshToken()
+    {
+        return refreshToken;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        RefreshCommand that = (RefreshCommand) o;
+        return refreshToken.equals(that.refreshToken);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(refreshToken);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "RefreshCommand{" +
+                "refreshToken='" + refreshToken + '\'' +
+                '}';
+    }
+}
